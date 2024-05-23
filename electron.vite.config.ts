9de39bca8ef2bfb7a9ed66cd434be8c312235224
buzhfianji <buzhifanji@163.com>
2024-05-23 16:24:31 +0800

    搭建环境

---
+++ electron.vite.config.ts
@@ -0,0 +1,26 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss]
+      }
+    }
+  }
+})
