f501c45c53a6e2be8274c2718b60481ab0b743df
buzhfianji <buzhifanji@163.com>
2024-05-30 10:40:20 +0800

    添加 i18n

--- src/renderer/src/view/setting/Setting.vue
+++ src/renderer/src/view/setting/Setting.vue
@@ -7,31 +7,48 @@ import { themeStore } from './theme';
 const selectedTheme = ref(themeStore.getDefault(themeStore.list[0]));
 const selectedLang = ref(languageStore.getDefault(languageStore.list[0]))
 
+const textOpacity = { '--tw-text-opacity': 0.6 };
+
 </script>
 
 <template>
-  <div class="p-6 grid grid-cols-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div class="p-6 grid grid-cols-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-screen-2xl">
     <div class="col-span-full">
       <h2 class="font-bold text-lg ">
-        用户设置
+        {{ $t('setting.userSetting') }}
       </h2>
     </div>
     <div class="col-span-full lg:col-auto">
-      <div class="card w-96 bg-base-100 shadow">
+      <div class="card w-full bg-base-100 shadow">
         <div class="card-body">
-          <h2 class="card-title">语言 & 主题</h2>
+          <h2 class="card-title">{{ $t('setting.language') }} & {{ $t('setting.theme') }}</h2>
           <label class="form-control w-full max-w-xs">
             <div class="label">
-              <span class="label-text">选择语言</span>
+              <span class="label-text" :style="textOpacity">{{ $t('setting.chooseLanguage') }}</span>
             </div>
             <Select v-model="selectedLang" :list="languageStore.list" @update:model-value="languageStore.set" />
           </label>
           <label class="form-control w-full max-w-xs">
             <div class="label">
-              <span class="label-text">选择主题</span>
+              <span class="label-text" :style="textOpacity">{{ $t('setting.chooseTheme') }}</span>
             </div>
             <Select v-model="selectedTheme" :list="themeStore.list" @update:model-value="themeStore.set" />
           </label>
+        </div>
+      </div>
+    </div>
+    <div class="col-span-full lg:col-span-2">
+      <div class="card bg-base-100 shadow w-full">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="cursor-pointer label">
+              <div>
+                <h3 class="font-bold text-xl">sssss</h3>
+                <p class="label-text font-normal" :style="textOpacity">Remember me</p>
+              </div>
+              <input type="checkbox" checked="checked" class="checkbox checkbox-info" />
+            </label>
+          </div>
         </div>
       </div>
     </div>
