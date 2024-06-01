ec8425b1f342adddef6602aa0a0ce0790ae5d3d6
buzhfianji <buzhifanji@163.com>
2024-06-01 17:22:20 +0800

    修改章节模式下 不会更新dom的bug

--- src/renderer/src/view/reader/SectionReader.vue
+++ src/renderer/src/view/reader/SectionReader.vue
@@ -54,7 +54,7 @@ function linkClick(href: string) {
       <div class="py-8">
         <button class="btn btn-active btn-neutral" @click="prev">上一章</button>
       </div>
-      <SectionView :data="section" @link-click="linkClick"></SectionView>
+      <SectionView :key="index" :data="section" @link-click="linkClick"></SectionView>
       <div class="text-center py-10">
         <button class="btn  btn-active  btn-primary btn-wide" @click="next">下一章</button>
       </div>
